f0a5167d2892e91b762a91a1bbc214a03ee407b7
AI091 <66860955+AI091@users.noreply.github.com>
2022-08-25 17:57:53 +0200

    Update recommender.py

--- app/modules/recommender.py
+++ app/modules/recommender.py
@@ -6,11 +6,12 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 class recommender:
     def __init__(self, data : list , features_label : str):
-        """[summary]
-
+        """
+        Initializes the data frame and prepares the the data for the model by using 
+        Count Vectorizer 
         Args:
-            data (DataFrame): [description]
-            features_label (str): [description]
+            data (DataFrame): [ Fetched from the database ]
+            features_label (str): [the label of the features column in the dataframe ]
         """
         self.data = DataFrame(data)
         self.features_label = features_label
@@ -19,10 +20,10 @@ class recommender:
         self.sim_matrix = self.fit()
 
     def vectorize(self):
-        """[summary]
-
+        """
+        applies the countVecctorizer mehtod
         Returns:
-            [type]: [description]
+            [self.count_matrix]: a matriz with frequency of the words in each movie
         """
         self.count_matrix = self.count_vectorizer.fit_transform(self.data[self.features_label])
         return self.count_matrix
@@ -30,22 +31,26 @@ class recommender:
     def print_count_matrix(self):
         print("Count Matrix:", self.count_matrix.toarray())
 
-    def print_sim_matrix(self):
-        """[summary]
-        """
-        print ("Sim Matrix:", self.sim_matrix)
-
+        
     def fit(self):
-        """[summary]
+        """Applies the cosine similarity model on the frequency matrix
 
         Returns:
-            [type]: [description]
+          self.sim_matrix: a similarity score matrix between each movie and otheres 
         """
         self.sim_matrix = cosine_similarity(self.count_matrix)
         return self.sim_matrix
+        
+    def print_sim_matrix(self):
+        """ 
+        Prints the similarity matrix
+        """
+        print ("Sim Matrix:", self.sim_matrix)
+
+    
 
     def recommend(self, id : str , n_recommendations : int = 1):
-        """[summary]
+        """ Recommends either one or three movies based on the user's request
 
         Args:
             id (str): [description]
@@ -55,10 +60,10 @@ class recommender:
             [type]: [description]
         """
         self.data.index.name = 'id'
-        z = pd.Series(self.data.index,index=self.data._id.values).to_dict()
-        y = pd.Series(self.data._id.values,index=self.data.index).to_dict()
-        m = list(enumerate(self.sim_matrix[z[id]]))
+        z = pd.Series(self.data.index,index=self.data._id.values).to_dict() # dictionary between index and id  
+        y = pd.Series(self.data._id.values,index=self.data.index).to_dict() # dictionary between id and index 
+        m = list(enumerate(self.sim_matrix[z[id]])) # similarity score calculted according to the id 
         m.sort(key=lambda x:x[1], reverse=True)
         m = m[1:n_recommendations+1]
-        return ( y[i[0]] for i in m)
+        return ( y[i[0]] for i in m) # returns the id of the movie for the database to fetch it 
 
